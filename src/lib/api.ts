f1eaca0ceff205bd176923da5031803e3975ac90
Diego Souza <diegosouza@tradecom.com.br>
2022-05-16 23:16:04 -0300

    Consumindo lista de feedbacks

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,5 +2,6 @@ import axios from "axios";
 
 console.log(import.meta.env)
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  // baseURL: import.meta.env.VITE_API_URL,
+  baseURL: 'http://localhost:3333',
 })
